e0ecce8d8ab0a6793773f2ba1c1218b5169da991
Michael Bush <budawolf333@gmail.com>
2015-12-12 00:04:26 -0500

    Initial

---
+++ Switcher/Assets/Map.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map : MonoBehaviour {
+    public Transform Path;
+
+    private MapGenerator Generator;
+
+	// Use this for initialization
+	void Start () {
+        Generator = new MapGenerator(10, 10);
+        Generator.Iterate((x, y) =>
+        {
+            var path = Instantiate(Path, new Vector3(x - 4.5f, 0, y - 4.5f), Quaternion.identity);
+        });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        	
+	}
+}
---
+++ Switcher/Assets/MapGenerator.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MapGenerator {
+
+    public int Width = 10;
+    public int Height = 10;
+
+    private int[,] Map;
+
+    public MapGenerator(int width, int height)
+    {
+        Width = width;
+        Height = height;
+
+        Map = new int[10, 10];
+
+        for(var i = 0; i < 10; i++)
+        {
+            Map[4, i] = 1;
+            Map[7, i] = 1;
+            Map[i, 4] = 1;
+        }
+    }
+
+    public void Iterate(Action<int, int> operation)
+    {
+        for(var x = 0; x < Width; x++)
+        {
+            for(var y = 0; y < Height; y++)
+            {
+                if (Map[x, y] == 1)
+                {
+                    operation(x, y);
+                }
+            }
+        }
+    }
+}
