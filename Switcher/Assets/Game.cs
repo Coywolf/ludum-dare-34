001a6019fd019729d40319e340ee691f04bfd50d
Michael Bush <budawolf333@gmail.com>
2015-12-13 15:23:44 -0500

    map generation woot. also intersections can't point to nothing

--- Switcher/Assets/Game.cs
+++ Switcher/Assets/Game.cs
@@ -34,57 +34,7 @@ public class Game : MonoBehaviour {
         GuiController.SetLives(Lives);
         GuiController.SetPoints(Points);
 
-        Intersections = new List<Assets.Intersection>();
-
-        Map = new Map(10, 10);
-        Map.Iterate(tile =>
-        {
-            Transform prefab = null;
-
-            if(tile is Assets.Path)
-            {
-                prefab = Path;
-            }
-            else if(tile is Assets.Spawn)
-            {
-                prefab = Spawn;
-            }
-            else if (tile is Assets.Exit)
-            {
-                prefab = Exit;
-            }
-            else if (tile is Assets.Intersection)
-            {
-                prefab = Intersection;
-            }
-
-            if (prefab != null)
-            {
-                var path = Instantiate(prefab, new Vector3(tile.x, 0, tile.y), Quaternion.identity) as Transform;
-                path.parent = transform;
-
-                if(prefab == Spawn)
-                {
-                    path.GetComponent<Spawner>().Initialize(tile.x, tile.y, Map);
-                }
-                else if(prefab == Intersection)
-                {
-                    Intersections.Add((Assets.Intersection)tile);
-                    ((Assets.Intersection)tile).Instance = path;
-                }
-                else if(prefab == Exit)
-                {
-                    foreach(var ren in path.GetChild(0).GetComponentsInChildren<Renderer>())
-                    {
-                        ren.material.color = ((Assets.Exit)tile).Color;
-                    }
-                }
-            }
-        });
-
-        SelectorInstance = (Transform) Instantiate(Selector, new Vector3(0, -10, 0), Quaternion.identity);
-        SelectorInstance.parent = transform;
-        SetSelectedIntersection(0);
+        CreateMap();
     }
 	
 	// Update is called once per frame
@@ -103,14 +53,87 @@ public class Game : MonoBehaviour {
 
         if (Input.GetButtonDown("Button2"))
         {
-            Intersections[SelectedIntersection].Direction++;
+            while (true)
+            {
+                Intersections[SelectedIntersection].Direction++;
+                var point = new Assets.Point(Intersections[SelectedIntersection].x, Intersections[SelectedIntersection].y);
+                var nextPoint = point.Move(Intersections[SelectedIntersection].Direction);
+                if(Map.GetTile(nextPoint.X, nextPoint.Y) != null)
+                {
+                    break;
+                }
+            }
 
             if (RotateClip != null)
             {
                 AudioSource.PlayClipAtPoint(RotateClip, Camera.main.transform.position);
             }
         }
+        if (Input.GetKeyDown("p"))
+        {
+            CreateMap();
+        }
 	}
+
+    private void CreateMap()
+    {
+        foreach(Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        Intersections = new List<Assets.Intersection>();
+
+        Map = new Map(10, 10);
+        Map.Iterate(tile =>
+        {
+            Transform prefab = null;
+
+            if (tile is Assets.Path)
+            {
+                prefab = Path;
+            }
+            else if (tile is Assets.Spawn)
+            {
+                prefab = Spawn;
+            }
+            else if (tile is Assets.Exit)
+            {
+                prefab = Exit;
+            }
+            else if (tile is Assets.Intersection)
+            {
+                prefab = Intersection;
+            }
+
+            if (prefab != null)
+            {
+                var path = Instantiate(prefab, new Vector3(tile.x, 0, tile.y), Quaternion.identity) as Transform;
+                path.parent = transform;
+
+                if (prefab == Spawn)
+                {
+                    path.GetComponent<Spawner>().Initialize(tile.x, tile.y, Map);
+                }
+                else if (prefab == Intersection)
+                {
+                    Intersections.Add((Assets.Intersection)tile);
+                    ((Assets.Intersection)tile).Instance = path;
+                }
+                else if (prefab == Exit)
+                {
+                    foreach (var ren in path.GetChild(0).GetComponentsInChildren<Renderer>())
+                    {
+                        ren.material.color = ((Assets.Exit)tile).Color;
+                    }
+                }
+            }
+        });
+
+        SelectorInstance = (Transform)Instantiate(Selector, new Vector3(0, -10, 0), Quaternion.identity);
+        SelectorInstance.parent = transform;
+        SetSelectedIntersection(0);
+    }
     
     public void DestroyUnit(Unit unit, int x, int y)
     {
